c74b7b46ce3a4cfe6b7619ebd5402e98d8fe7ca2
Christian Weyer <christian.weyer@thinktecture.com>
2021-02-07 20:50:00 +0100

    Upgrade to MudBlazor 5.0; .NET packages upgrade

--- Client/Program.cs
+++ Client/Program.cs
@@ -3,7 +3,6 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using MudBlazor;
 using MudBlazor.Services;
 
 namespace MudBlazorDemoWasm.Client
@@ -17,9 +16,7 @@ namespace MudBlazorDemoWasm.Client
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            builder.Services.AddMudBlazorDialog();
-            builder.Services.AddMudBlazorSnackbar();
-            builder.Services.AddMudBlazorResizeListener();
+            builder.Services.AddMudServices();
 
             await builder.Build().RunAsync();
         }
